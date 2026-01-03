a2d2637c5789b070fb8737ac6a832bb911ecd53a
Sabio <sabiohy@gmail.com>
2026-01-03 02:11:31 -0500

    Initial Commit

---
+++ Music App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music App
+//
+//  Created by Mary Yousif on 2026-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music App/Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppApp.swift
+//  Music App
+//
+//  Created by Mary Yousif on 2026-01-03.
+//
+
+import SwiftUI
+
+@main
+struct Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
